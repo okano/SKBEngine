74d4fbd848e11bc60d6c279538e5b14227d8cd68
okano <okano@synergysoftware.jp>
2011-07-28 12:47:21 +0900

    add class "SKBE_PdfScrollView2" for base to MyPdfScrollView class.

---
+++ SKBEngine/SKBE_PdfScrollView2.h
@@ -0,0 +1,16 @@
+//
+//  SKBE_PdfScrollView2.h
+//  SKBEngine
+//
+//  Created by okano on 11/07/28.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SKBE_Utility.h"
+
+@interface SKBE_PdfScrollView2 : UIScrollView <UIScrollViewDelegate>  {
+    
+}
+
+@end
---
+++ SKBEngine/SKBE_PdfScrollView2.m
@@ -0,0 +1,14 @@
+//
+//  SKBE_PdfScrollView2.m
+//  SKBEngine
+//
+//  Created by okano on 11/07/28.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SKBE_PdfScrollView2.h"
+
+
+@implementation SKBE_PdfScrollView2
+
+@end
